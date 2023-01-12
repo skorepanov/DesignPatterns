cf8fdc3c5889b8f26050184f6811c2b4247c8ca1
Sergey Korepanov <igzhestik@mail.ru>
2023-01-12 17:06:39 +0500

    Add Abstract Factory

---
+++ AbstractFactory/LogSaver.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Common;
+
+namespace AbstractFactory;
+
+public class LogSaver
+{
+    private readonly DbProviderFactory _factory;
+
+    public LogSaver(DbProviderFactory factory)
+    {
+        _factory = factory;
+    }
+
+    public void Save(IEnumerable<LogEntry> logEntries)
+    {
+        using var connection = _factory.CreateConnection();
+        using var command = _factory.CreateCommand();
+        SetCommandParameters(command, logEntries);
+        command?.ExecuteNonQuery();
+    }
+
+    private void SetCommandParameters(DbCommand? command, IEnumerable<LogEntry> logEntries) { }
+}
+
+public class LogEntry { }
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.SqlClient;
+using Npgsql;
+using System.Data.Common;
+using AbstractFactory;
+
+List<LogEntry> logEntries = new List<LogEntry>();
+
+DbProviderFactory sqlFactory = SqlClientFactory.Instance;
+LogSaver sqlLogSaver = new LogSaver(sqlFactory);
+sqlLogSaver.Save(logEntries);
+
+DbProviderFactory npgsqlFactory = NpgsqlFactory.Instance;
+LogSaver npgsqlLogSaver = new LogSaver(npgsqlFactory);
+npgsqlLogSaver.Save(logEntries);
