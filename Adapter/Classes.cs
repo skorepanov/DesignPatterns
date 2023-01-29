555c4352048244fd222b7360fbe5ba641881fe12
Sergey Korepanov <igzhestik@mail.ru>
2023-01-30 01:41:55 +0500

    Add Adapter

---
+++ Adapter/Classes.cs
@@ -0,0 +1,59 @@
+﻿namespace Adapter;
+
+#region Log entry
+public abstract class LogEntry
+{
+    public DateTime EntryDateTime { get; set; }
+    public string Message { get; set; }
+}
+
+public class SimpleLogEntry : LogEntry
+{
+    public int Severity { get; set; }
+}
+
+public class ExceptionLogEntry : LogEntry
+{
+    public Exception Exception { get; set; }
+}
+#endregion
+
+#region Log saver
+public interface IAsyncLogSaver
+{
+    Task SaveAsync(LogEntry logEntry);
+}
+
+public class SqlServerAsyncLogSaverAdapter : IAsyncLogSaver
+{
+    private readonly SqlServerLogSaver _sqlServerLogSaver = new SqlServerLogSaver();
+
+    public Task SaveAsync(LogEntry logEntry)
+    {
+        switch (logEntry)
+        {
+            case SimpleLogEntry simpleLogEntry:
+                return Task.Run(() => _sqlServerLogSaver.Save(simpleLogEntry.EntryDateTime,
+                    simpleLogEntry.Severity.ToString(), simpleLogEntry.Message));
+            case ExceptionLogEntry exceptionEntry:
+                return Task.Run(() => _sqlServerLogSaver.SaveException(exceptionEntry.EntryDateTime,
+                    exceptionEntry.Message, exceptionEntry.Exception));
+        }
+
+        throw new ArgumentException();
+    }
+}
+
+public class SqlServerLogSaver
+{
+    public void Save(DateTime dateTime, string severity, string message)
+    {
+        Console.WriteLine($"Saving log entry: dateTime: {dateTime}, severity: {severity}, message: {message}.");
+    }
+
+    public void SaveException(DateTime dateTime, string message, Exception exception)
+    {
+        Console.WriteLine($"Saving exception log entry: dateTime: {dateTime}, message: {message}, exception: {exception.Message}.");
+    }
+}
+#endregion
---
+++ Adapter/Program.cs
@@ -0,0 +1,19 @@
+﻿using Adapter;
+
+IAsyncLogSaver adapter = new SqlServerAsyncLogSaverAdapter();
+
+var simpleLogEntry = new SimpleLogEntry
+{
+    EntryDateTime = DateTime.Now,
+    Message = "Log message",
+    Severity = 1,
+};
+await adapter.SaveAsync(simpleLogEntry);
+
+var exceptionLogEntry = new ExceptionLogEntry
+{
+    EntryDateTime = DateTime.Now,
+    Exception = new Exception("Test exception"),
+    Message = "Exception message",
+};
+await adapter.SaveAsync(exceptionLogEntry);
