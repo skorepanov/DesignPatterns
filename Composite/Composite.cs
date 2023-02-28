6f24596e59be10c87d8ac78ab85a546e7c0c1631
Sergey Korepanov <igzhestik@mail.ru>
2023-02-28 15:12:26 +0500

    Add Composite

---
+++ Composite/Composite.cs
@@ -0,0 +1,62 @@
+﻿namespace CompositePattern;
+
+public interface IComponent
+{
+    void DisplayPrice();
+    int GetTotalPrice();
+}
+
+public class Leaf : IComponent
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+
+    public Leaf(string name, int price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public void DisplayPrice()
+    {
+        Console.WriteLine($"{Name}: {Price}");
+    }
+
+    public int GetTotalPrice()
+    {
+        return Price;
+    }
+}
+
+public class Composite : IComponent
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    private List<IComponent> _components = new List<IComponent>();
+
+    public Composite(string name, int price)
+    {
+        this.Name = name;
+        this.Price = price;
+    }
+
+    public void AddComponent(IComponent component)
+    {
+        _components.Add(component);
+    }
+
+    public void DisplayPrice()
+    {
+        Console.WriteLine($"{Name}: {Price} (total price: {GetTotalPrice()})");
+
+        foreach (var component in _components)
+        {
+            component.DisplayPrice();
+        }
+    }
+
+    public int GetTotalPrice()
+    {
+        return Price + _components.Sum(c => c.GetTotalPrice());
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,37 @@
+﻿using CompositePattern;
+
+// создать простые компоненты
+IComponent cpu = new Leaf("CPU", 10000);
+IComponent ram = new Leaf("RAM", 8000);
+IComponent hardDisk = new Leaf("Hard disk", 4000);
+IComponent mouse = new Leaf("Mouse", 500);
+IComponent keyboard = new Leaf("Keyboard", 800);
+
+// создать составные компоненты
+Composite motherboard = new Composite("Motherboard", 5000);
+motherboard.AddComponent(cpu);
+motherboard.AddComponent(ram);
+
+Composite cabinet = new Composite("Cabinet", 2000);
+cabinet.AddComponent(motherboard);
+cabinet.AddComponent(hardDisk);
+
+Composite peripherals = new Composite("Peripherals", 0);
+peripherals.AddComponent(mouse);
+peripherals.AddComponent(keyboard);
+
+Composite computer = new Composite("Computer", 0);
+computer.AddComponent(cabinet);
+computer.AddComponent(peripherals);
+
+// вывести результат
+displayPrice(computer);
+Console.WriteLine("------------------------");
+displayPrice(motherboard);
+Console.WriteLine("------------------------");
+displayPrice(ram);
+
+void displayPrice(IComponent component)
+{
+    component.DisplayPrice();
+}
