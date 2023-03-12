71a3990681186f7fe7cbd69039e393fdf0d402ce
Sergey Korepanov <igzhestik@mail.ru>
2023-03-12 21:50:56 +0500

    Add Decorator

---
+++ Decorator/Classes.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+
+namespace Decorator;
+
+public class LogEntry
+{
+    public DateTime EntryDateTime { get; set; }
+    public string Message { get; set; }
+}
+
+// component
+public interface ILogSaver
+{
+    Task SaveLogEntry(string applicationId, LogEntry logEntry);
+}
+
+// concrete component
+public sealed class PostgresqlLogSaver : ILogSaver
+{
+    public Task SaveLogEntry(string applicationId, LogEntry logEntry)
+    {
+        Console.WriteLine("PostgresqlLogSaver.SaveLogEntry()");
+
+        // сохранить переданную запись в Postgresql
+        return Task.FromResult<object>(result: null);
+    }
+}
+
+// base decorator
+public abstract class LogSaverDecorator : ILogSaver
+{
+    protected readonly ILogSaver _logSaver;
+
+    protected LogSaverDecorator(ILogSaver logSaver)
+    {
+        this._logSaver = logSaver;
+    }
+
+    public abstract Task SaveLogEntry(string applicationId, LogEntry logEntry);
+}
+
+// concrete decorator 1
+public class ThrottlingLogSaverDecorator : LogSaverDecorator
+{
+    public ThrottlingLogSaverDecorator(ILogSaver logSaver)
+        : base(logSaver) { }
+
+    public override async Task SaveLogEntry(string applicationId, LogEntry logEntry)
+    {
+        Console.WriteLine("ThrottlingLogSaverDecorator.SaveLogEntry()");
+
+        if (IsQuotaReached(applicationId))
+        {
+            // лимит приложения исчерпан, сохранение невозможно
+            throw new QuotaReachedException();
+        }
+
+        IncrementUsedQuota(applicationId);
+
+        await _logSaver.SaveLogEntry(applicationId, logEntry);
+    }
+
+    private bool IsQuotaReached(string applicationId)
+    {
+        // проверить, израсходована ли квота приложения
+        return false;
+    }
+
+    private void IncrementUsedQuota(string applicationId)
+    {
+        // увеличить число поступивших запросов
+    }
+}
+
+public class QuotaReachedException : Exception { }
+
+// concrete decorator 2
+public class TraceLogSaverDecorator : LogSaverDecorator
+{
+    public TraceLogSaverDecorator(ILogSaver logSaver)
+        : base(logSaver) { }
+
+    public override async Task SaveLogEntry(string applicationId, LogEntry logEntry)
+    {
+        Console.WriteLine("TraceLogSaverDecorator.SaveLogEntry()");
+
+        var stopwatch = Stopwatch.StartNew();
+
+        try
+        {
+            await _logSaver.SaveLogEntry(applicationId, logEntry);
+        }
+        finally
+        {
+            Console.WriteLine($"Операция сохранения завершена за {stopwatch.ElapsedMilliseconds} мс.");
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,16 @@
+﻿using Decorator;
+
+// использование одного декоратора
+ILogSaver logSaver = new ThrottlingLogSaverDecorator(
+    new PostgresqlLogSaver());
+
+logSaver.SaveLogEntry(applicationId: "testApp", logEntry: new LogEntry());
+
+Console.WriteLine();
+
+// использование нескольких декораторов
+logSaver = new ThrottlingLogSaverDecorator(
+    new TraceLogSaverDecorator(
+        new PostgresqlLogSaver()));
+
+logSaver.SaveLogEntry(applicationId: "testApp", logEntry: new LogEntry());
