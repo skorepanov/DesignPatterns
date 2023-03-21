0882a972a353ba95b57b92e8668b9775d82238d4
Sergey Korepanov <igzhestik@mail.ru>
2023-03-21 14:49:54 +0500

    Add Flyweight

---
+++ Flyweight/Classes.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+namespace Flyweight;
+
+public abstract class House
+{
+    protected Guid _id;
+    protected int FloorsCount;
+
+    protected House()
+    {
+        _id = Guid.NewGuid();
+    }
+
+    public abstract void Build(decimal latitude, decimal longitude);
+}
+
+public class PanelHouse : House
+{
+    public PanelHouse()
+    {
+        FloorsCount = 16;
+    }
+
+    public override void Build(decimal latitude, decimal longitude)
+    {
+        Console.WriteLine($"Построен панельный дом из {FloorsCount} этажей (id = {_id}); "
+            + $"координаты: {latitude} широты, {longitude} долготы.");
+    }
+}
+
+public class BrickHouse : House
+{
+    public BrickHouse()
+    {
+        FloorsCount = 5;
+    }
+
+    public override void Build(decimal latitude, decimal longitude)
+    {
+        Console.WriteLine($"Построен кирпичный дом из {FloorsCount} этажей (id = {_id}); "
+                          + $"координаты: {latitude} широты, {longitude} долготы.");
+    }
+}
+
+public class HouseFactory
+{
+    private readonly Hashtable _houses = new Hashtable();
+
+    public House GetHouse(string key)
+    {
+        var house = _houses[key] as House;
+
+        if (house is null)
+        {
+            house = key switch
+            {
+                "Panel" => new PanelHouse(),
+                "Brick" => new BrickHouse(),
+                _ => house
+            };
+            _houses.Add(key, house);
+        }
+
+        return house;
+    }
+}
---
+++ Flyweight/Program.cs
@@ -0,0 +1,25 @@
+﻿using Flyweight;
+
+var latitude = 10.0m;
+var longitude = 20.0m;
+var houseFactory = new HouseFactory();
+
+for (var i = 0; i < 5; i++)
+{
+    var panelHouse = houseFactory.GetHouse("Panel");
+    panelHouse.Build(latitude, longitude);
+
+    latitude += 0.1m;
+    longitude += 0.2m;
+}
+
+Console.WriteLine();
+
+for (var i = 0; i < 5; i++)
+{
+    var brickHouse = houseFactory.GetHouse("Brick");
+    brickHouse.Build(latitude, longitude);
+
+    latitude += 0.1m;
+    longitude += 0.2m;
+}
