cf5f18c978014d38505687e43e8e895463709ff8
Sergey Korepanov <igzhestik@mail.ru>
2023-03-30 01:46:32 +0500

    Add Bridge

---
+++ Bridge/Classes.cs
@@ -0,0 +1,77 @@
+﻿namespace Bridge;
+
+#region Messages (Abstractions)
+// Abstraction
+public abstract class Message
+{
+    public IMessageSender MessageSender { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public abstract void Send();
+}
+
+// Refined abstraction 1
+public class SystemMessage : Message
+{
+    public override void Send()
+    {
+        MessageSender.SendMessage(Subject, Body);
+    }
+}
+
+// Refined abstraction 2
+public class UserMessage : Message
+{
+    public string UserComments { get; set; }
+
+    public override void Send()
+    {
+        var fullBody = $"{Body}{Environment.NewLine}User comments: {UserComments}";
+        MessageSender.SendMessage(Subject, fullBody);
+    }
+}
+#endregion
+
+#region MessageSenders (Implementors)
+// Implementor (Bridge)
+public interface IMessageSender
+{
+    void SendMessage(string subject, string body);
+}
+
+// Concrete implementor 1
+public class EmailSender : IMessageSender
+{
+    public void SendMessage(string subject, string body)
+    {
+        Console.WriteLine(
+              $"By email{Environment.NewLine}"
+            + $"Subject: {subject}{Environment.NewLine}"
+            + $"Body: {body}{Environment.NewLine}");
+    }
+}
+
+// Concrete implementor 2
+public class MqSender : IMessageSender
+{
+    public void SendMessage(string subject, string body)
+    {
+        Console.WriteLine(
+              $"By MQ{Environment.NewLine}"
+            + $"Subject: {subject}{Environment.NewLine}"
+            + $"Body: {body}{Environment.NewLine}");
+    }
+}
+
+// Concrete implementor 3
+public class WebServiceSender : IMessageSender
+{
+    public void SendMessage(string subject, string body)
+    {
+        Console.WriteLine(
+              $"By WebService{Environment.NewLine}"
+            + $"Subject: {subject}{Environment.NewLine}"
+            + $"Body: {body}{Environment.NewLine}");
+    }
+}
+#endregion
---
+++ Bridge/Program.cs
@@ -0,0 +1,26 @@
+﻿using Bridge;
+
+IMessageSender email = new EmailSender();
+IMessageSender mq = new MqSender();
+IMessageSender webService = new WebServiceSender();
+
+Message systemMessage = new SystemMessage();
+systemMessage.Subject = "System subject";
+systemMessage.Body = "System body";
+
+systemMessage.MessageSender = email;
+systemMessage.Send();
+
+systemMessage.MessageSender = mq;
+systemMessage.Send();
+
+systemMessage.MessageSender = webService;
+systemMessage.Send();
+
+UserMessage userMessage = new UserMessage();
+userMessage.Subject = "User subject";
+userMessage.Body = "User body";
+userMessage.UserComments = "Some comments";
+
+userMessage.MessageSender = email;
+userMessage.Send();
