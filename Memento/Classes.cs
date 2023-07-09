a6a7a10215b27f2f487d2aa31f874c917c76d024
Sergey Korepanov <igzhestik@mail.ru>
2023-07-10 00:20:42 +0500

    Add Memento

---
+++ Memento/Classes.cs
@@ -0,0 +1,71 @@
+﻿namespace Memento_Simple;
+
+/// <summary>
+/// Originator (Создатель)
+/// </summary>
+public class Document
+{
+    private string _text = string.Empty;
+    private int _style = 1;
+
+    public void AddBlock(string text)
+    {
+        _text += text + Environment.NewLine;
+    }
+
+    public void SetStyle(int style)
+    {
+        _style = style;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Стиль: {_style}");
+        Console.WriteLine("Текст:");
+        Console.WriteLine(_text);
+    }
+
+    public DocumentMemento SaveState()
+    {
+        return new DocumentMemento(_text, _style);
+    }
+
+    public void RestoreState(DocumentMemento memento)
+    {
+        _text = memento.Text;
+        _style = memento.Style;
+    }
+}
+
+/// <summary>
+/// Снимок
+/// </summary>
+public class DocumentMemento
+{
+    public string Text { get; }
+    public int Style { get; }
+
+    public DocumentMemento(string text, int style)
+    {
+        Text = text;
+        Style = style;
+    }
+}
+
+/// <summary>
+/// Caretaker (Опекун)
+/// </summary>
+public class EditorHistory
+{
+    private readonly Stack<DocumentMemento> _history = new();
+
+    public void Push(DocumentMemento memento)
+    {
+        _history.Push(memento);
+    }
+
+    public DocumentMemento Pop()
+    {
+        return _history.Pop();
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,18 @@
+﻿using Memento_Simple;
+
+var document = new Document();
+var history = new EditorHistory();
+
+document.AddBlock("Hello, world!");
+document.SetStyle(2);
+document.Print();
+var stateToSave = document.SaveState();
+history.Push(stateToSave);
+
+document.AddBlock("Goodbye, world!");
+document.SetStyle(3);
+document.Print();
+
+var stateToRestore = history.Pop();
+document.RestoreState(stateToRestore);
+document.Print();
